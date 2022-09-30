8699029bf220d654c28d0bcb4b34c41273dd45f8
arnold-O <arnoldoghiator@gmail.com>
2022-09-30 22:56:06 +0100

    setting up server

---
+++ node-102/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
---
+++ node-102/server.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB, {}).then((conn) => {
+  // console.log(conn.connection);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`port running on ${PORT}`);
+});
