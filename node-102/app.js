ef3473a46b84d32cbac7da4bc2c84206e5da2925
arnold-O <arnoldoghiator@gmail.com>
2022-10-04 22:52:46 +0100

    global error

--- node-102/app.js
+++ node-102/app.js
@@ -1,40 +1,32 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
-const tourRoutes = require('./route/tourRoutes')
+const tourRoutes = require("./route/tourRoutes");
 
-
-const app = express()
-app.use(express.json())
-
-
-
-
-
+const app = express();
+app.use(express.json());
 
 // ROUTES
-app.use('/api/v1/tour', tourRoutes)
-
+app.use("/api/v1/tour", tourRoutes);
 
 // Error-handler
-app.all('*', (req, res, next)=>{
-    res.status(404).json({
-        status:'fail',
-        message:`can't find ${req.originalUrl} on this server`
-    })
-})
+app.all("*", (req, res, next) => {
+//   res.status(404).json({
+//     status: "fail",
+//     message: `can't find ${req.originalUrl} on this server`,
+//   });
+const err = new Error(`can't find ${req.originalUrl} on this server`)
+next(err)
+});
 
+app.use((err, req, res, next) => {
 
-app.use((err, req, res, next)=>{
-
-    res.status(200).json({
-        
-    })
-})
-
-
-
-
-
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status:err.status,
+    message:err.message,
+  });
+});
 
 module.exports = app;
