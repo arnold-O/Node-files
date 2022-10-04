4ea57cd8a67309f309ac5e856ba52405038e3738
arnold-O <arnoldoghiator@gmail.com>
2022-10-04 17:27:10 +0100

    agggregation pipeline II

--- node-102/controllers/tourController.js
+++ node-102/controllers/tourController.js
@@ -77,19 +77,18 @@ exports.deleteTour = async (req, res, next) => {
 exports.gettourStats = async (req, res, next) => {
   try {
     const stat = await Tour.aggregate([
-    
       {
         $group: {
-          _id:'$difficulty',
-          numTours:{$sum:1},
-          avgPrice: { $avg: '$price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' },
+          _id: "$difficulty",
+          numTours: { $sum: 1 },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
         },
       },
       {
-        $sort:{avgPrice:1}
-      }
+        $sort: { avgPrice: 1 },
+      },
     ]);
     res.status(200).json({
       stat,
@@ -97,4 +96,26 @@ exports.gettourStats = async (req, res, next) => {
   } catch (error) {
     res.status(200).json(error);
   }
+};
+
+exports.getMonthlyPlan = async (req, res, next) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+    ]);
+    res.status(200).json({
+      plan,
+    });
+  } catch (error) {}
 };
--- node-102/route/tourRoutes.js
+++ node-102/route/tourRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { createTour, getAllTour, getTour, updateTour, deleteTour, gettourStats } = require('../controllers/tourController')
+const { createTour, getAllTour, getTour, updateTour, deleteTour, gettourStats, getMonthlyPlan } = require('../controllers/tourController')
 
 
 const router = express.Router()
 
 
 router.get('/getstats', gettourStats)
+router.get('/monthly-plan/:year', getMonthlyPlan)
 
 router.post('/create', createTour)
 router.get('/getall', getAllTour)
