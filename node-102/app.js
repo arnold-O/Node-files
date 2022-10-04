a1f413440ded7b54fbea3f089095a68eb6212e8e
arnold-O <arnoldoghiator@gmail.com>
2022-10-04 20:41:57 +0100

    global error

--- node-102/app.js
+++ node-102/app.js
@@ -12,12 +12,25 @@ app.use(express.json())
 
 
 
-
-
+// ROUTES
 app.use('/api/v1/tour', tourRoutes)
 
 
+// Error-handler
+app.all('*', (req, res, next)=>{
+    res.status(404).json({
+        status:'fail',
+        message:`can't find ${req.originalUrl} on this server`
+    })
+})
 
+
+app.use((err, req, res, next)=>{
+
+    res.status(200).json({
+        
+    })
+})
 
 
 
