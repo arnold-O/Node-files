edf4145617e1d896bc0dfabda64f5fdff88c55a6
arnold-O <arnoldoghiator@gmail.com>
2022-10-05 00:34:00 +0100

    global error implimentation

--- node-102/app.js
+++ node-102/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 const tourRoutes = require("./route/tourRoutes");
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 
 const app = express();
 app.use(express.json());
@@ -11,22 +13,10 @@ app.use("/api/v1/tour", tourRoutes);
 
 // Error-handler
 app.all("*", (req, res, next) => {
-//   res.status(404).json({
-//     status: "fail",
-//     message: `can't find ${req.originalUrl} on this server`,
-//   });
-const err = new Error(`can't find ${req.originalUrl} on this server`)
-next(err)
+
+next(new AppError(`can't find ${req.originalUrl} on this server`, 404))
 });
 
-app.use((err, req, res, next) => {
-
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status:err.status,
-    message:err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
