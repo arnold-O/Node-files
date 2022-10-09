5663ab4dde3818b640c8a01c1e7a77b588f44983
arnold-O <arnoldoghiator@gmail.com>
2022-10-09 23:56:41 +0100

    token funtionality

--- node-102/controllers/authController.js
+++ node-102/controllers/authController.js
@@ -1,11 +1,23 @@
 const User = require("../model/userModel");
 const catchAsync = require("../utils/catchAsync");
+const jwt = require('jsonwebtoken')
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
 
+    const {name, email, password, passwordConfirm} = req.body
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm
+  });
+
+const token = jwt.sign({id:newUser._id}, process.env.JWT_SECRET,{
+    expiresIn:process.env.JWT_EXPIRES_IN
+})
   res.status(201).json({
     status: "success",
+    token,
     user: newUser,
   });
 });
@@ -14,7 +26,8 @@ exports.signUp = catchAsync(async (req, res, next) => {
 
 
 exports.login = catchAsync( async(req, res, next)=>{
-    
+
+
 }
 
 )
