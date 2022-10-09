5bcc1eddea049386cfe7ee9a0e1f6a39f7ec8dbe
arnold-O <arnoldoghiator@gmail.com>
2022-10-09 23:08:27 +0100

    user hashed password

--- node-102/controllers/authController.js
+++ node-102/controllers/authController.js
@@ -1,18 +1,20 @@
-const User = require('../model/userModel')
-const catchAsync = require('../utils/catchAsync')
+const User = require("../model/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    user: newUser,
+  });
+});
 
 
 
 
+exports.login = catchAsync( async(req, res, next)=>{
+    
+}
 
-exports.signUp = catchAsync(  async(req, res, next)=>{
-    const newUser = await User.create(
-        req.body
-
-    )
-
-    res.status(201).json({
-        status:"success",
-        user:newUser
-    })
-})
+)
--- node-102/model/userModel.js
+++ node-102/model/userModel.js
@@ -1,39 +1,47 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-
-
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required: [true, 'please tell us your name']
+  name: {
+    type: String,
+    required: [true, "please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "please provide a passord"],
+    minlength: 6,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "please comfim your password"],
+    validate: {
+      // this works onl SAVE & Create
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "passwords are not the same",
     },
-    email:{
+  },
+  photo: String,
+});
 
-        type:String,
-        required:[true, 'please provide your email'],
-        unique:true,
-        lowercase:true,
-        validate:[ validator.isEmail, 'please provide a valid email']
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    },
-    password:{
-        type:String,
-        required:[true, 'please provide a passord'],
-        minlength:6
-    },
-    passwordConfirm:{
-        type:String,
-        required:[true, 'please comfim your password']
-    },
-    photo:String,
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
 
+  next();
+});
 
+const User = mongoose.model("User", userSchema);
 
-})
-
-
-
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = User;
