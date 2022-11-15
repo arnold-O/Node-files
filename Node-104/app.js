e6d745cdd71ac3b8d26544a3a3e5b96357eb3a06
arnold-O <arnoldoghiator@gmail.com>
2022-11-15 13:14:56 +0100

    database connection

--- Node-104/app.js
+++ Node-104/app.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require("express");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000;
+
+// DAtabase connection
+const connectDB = require("./db/connectDB")
 
 
+async function start() {
+  try {
+    await connectDB(process.env.DATABASE);
+    app.listen(port, console.log(`server listening on port ${port}.....`))
+  } catch (error) {
+
+  }
+}
 
 
+start()
