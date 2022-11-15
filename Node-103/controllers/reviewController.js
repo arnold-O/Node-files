988fb2c4036dc97d99b957d03970f83c919f59b9
arnold-O <arnoldoghiator@gmail.com>
2022-11-15 12:53:51 +0100

    project four setup

--- Node-103/controllers/reviewController.js
+++ Node-103/controllers/reviewController.js
@@ -2,76 +2,60 @@ const Review = require("../models/Review");
 const asyncHandler = require("../utils/asyncWrapper");
 const ErrorResponse = require("../utils/errorResponse");
 
-
-
-
-
-
 // @desc     Create  Review
 // @route   Post /api/v1/review
 // @access   Private/User/Admin
-exports.createReview = asyncHandler(async (req, res, next)=>{
-
-    const review =  await Review.create(req.body)
-})
-
+exports.createReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.create(req.body);
+});
 
 // @desc     Get  Reviews
 // @route   Get /api/v1/bootcamps/:bootcampId/review
 // @access   Public
-exports.getAllReview = asyncHandler(async (req, res, next)=>{
-
-    if(req.params.bootcampId){
-
-        const review =  await Review.find({bootcamp : req.params.bootcampId})
-        return res.status(200).json({
-            success:"success",
-            count: review.length,
-            review
-        });
-    }else{
-
-        res.status(200).json(res.advanceResult)
-    }
-
-})
-
-
+exports.getAllReview = asyncHandler(async (req, res, next) => {
+  if (req.params.bootcampId) {
+    const review = await Review.find({ bootcamp: req.params.bootcampId });
+    return res.status(200).json({
+      success: "success",
+      count: review.length,
+      review,
+    });
+  } else {
+    res.status(200).json(res.advanceResult);
+  }
+});
 
 // @desc     Get  Review
 // @route   Get /api/v1/review/:id
 // @access   Public
-exports.getRevieew = asyncHandler(async (req, res, next)=>{
+exports.getRevieew = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({
+    path: " bootcamp",
+    select: "name description",
+  });
+  if (!review) {
+    return next(
+      new ErrorResponse(`no review found with the Id ${req.params.id}`, 404)
+    );
+  }
 
-    const review =  await Review.findById(req.params.id).populate({
-        path:" bootcamp",
-        select:"name description"
-    })
-    if(!review){
-        return next(new ErrorResponse(`no review found with the Id ${req.params.id}`, 404))
-    }
-
-
-    res.status(200).json({
-        status:"success",
-        review
-    })
-})
-
+  res.status(200).json({
+    status: "success",
+    review,
+  });
+});
 
 // @desc     Update  Review
 // @route   Delete /api/v1/review:id
 // @access   Private/Admin
 
-exports.updateReview = asyncHandler(async (req, res, next)=>{
-
-    const review =  await Review.create(req.body)
-})
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.create(req.body);
+});
 
 // @desc     Delete  User
 // @route   Delete /api/v1/review/:id
 // @access   Private/Admin
-exports.deleteReview = asyncHandler(async (req, res, next)=>{
-
-    const review =  await Review.create(req.body)
-})
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.create(req.body);
+});
--- Node-103/routes/review.js
+++ Node-103/routes/review.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const { getRevieew, createReview, updateReview, deleteReview, getAllReview } = require("../controllers/reviewController");
-const appFeatures = require('../middleware/appFeatures');
-const { authorize, protect } = require('../middleware/protected');
-const Review = require('../models/Review');
-
+const express = require("express");
+const {
+  getRevieew,
+  createReview,
+  updateReview,
+  deleteReview,
+  getAllReview,
+} = require("../controllers/reviewController");
+const appFeatures = require("../middleware/appFeatures");
+const { authorize, protect } = require("../middleware/protected");
+const Review = require("../models/Review");
 
 const router = express.Router({ mergeParams: true });
 
-router.get(
-  "/",
-  appFeatures(Review),
-  getAllReview
-  );
-  
-  router.use(protect);
-  router.use(authorize('admin', 'user'))
+router.get("/", appFeatures(Review), getAllReview);
+
+router.use(protect);
+router.use(authorize("admin", "user"));
 // router.post("/create", createBootcamp);
 
 router.get("/:id", getRevieew);
-router.post("/",  createReview);
-router.patch("/:id",  updateReview);
-router.delete("/:id",  deleteReview);
+router.post("/", createReview);
+router.patch("/:id", updateReview);
+router.delete("/:id", deleteReview);
 
 module.exports = router;
