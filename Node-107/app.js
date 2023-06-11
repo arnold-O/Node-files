184c364c591e4d82434458e704dc6b53f982dd95
arnold-O <arnoldoghiator@gmail.com>
2023-06-11 08:05:31 +0100

    user model added

--- Node-107/app.js
+++ Node-107/app.js
@@ -1,46 +1,115 @@
-require('dotenv').config()
-require('./config/database').connect()
-const express = require('express')
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const cookieparser = require('cookie-parser')
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+const jwt = require('jsonwebtoken')
+const auth = require('./middleware/auth')
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cookieparser());
 
-const User = require('./models/User')
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const User = require("./models/User");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.post("/register", async (req, res) => {
+ try {
+    const { firstname, lastname, email, password } = req.body;
 
-
-
-
-
-app.post('/register', async (req, res)=>{
-    const {firstname, lastname, email, password} = req.body;
-
-
-    if(!firstname || !lastname || !email || !password){
-        res.status(400).json({
-            message:"Provide all fields"
-        })
+    if (!firstname || !lastname || !email || !password) {
+    return   res.status(400).json({
+        message: "Provide all fields",
+      });
     }
+  
+    const existUser = await User.findOne({ email });
+  
+    if (existUser) {
+    return   res.status(401).json({
+        message: "User already exist",
+      });
+    }
+  
+    const newUser = await User.create({
+      firstname,
+      lastname,
+      email,
+      password,
+    });
+  
+        const token = jwt.sign({user_id :newUser._id}, process.env.SECRET_KEY_JWT ,{expiresIn:"2h"})
+        newUser.token = token;
+        newUser.password = undefined;
+
+    res.status(201).json({
+      newUser
+    });
     
-   const existUser = await User.findOne({email})
+ } catch (error) {
+
+    console.log(error)
+    
+ }
+});
 
 
-   if(existUser){
-    res.status(401).json({
-        message:"User already exist"
-    })
+app.post('/login', async(req, res)=>{
+    try {
+        const {email, password} = req.body
 
-   }
 
+        if(!email || !password){
+            return res.status(400).json({
+                message:"Fields Missing"
+            })
+        }
+
+       const checkUser = await User.findOne({email})
+
+       if(!checkUser){
+       
+         return res.status(400).json({
+            message:"User does not exist"
+        })
+       }
+
+
+      const passwordChecker = await checkUser.matchPassword(password)
+      if(!passwordChecker){
+        return res.status(400).json({
+            message:"Invalid credentials"
+        })
+
+      }else{
+        const token = jwt.sign({user_id: checkUser._id, email}, process.env.SECRET_KEY_JWT, {expiresIn:"3d"})
+        checkUser.token = token;
+        checkUser.password = undefined;
+
+    //   return  res.status(200).json(checkUser)
+    const options = {
+        expires:new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly:true
+    }
+            res.status(200).cookie('token', token, options).json({
+                message:"Login Successfully",
+                checkUser
+            })
+      }
+
+        
+    } catch (error) {
+
+        console.log(error)
+        
+    }
+})
+app.get('/dash', auth, (req, res)=>{
+
+    res.send('Secret path')
 
 })
 
-
-
-
-
-module.exports = app
+module.exports = app;
--- Node-107/models/User.js
+++ Node-107/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   firstname: {
@@ -13,12 +14,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     unique: true,
   },
-  passsword: {
+  password: {
     type: String,
   },
   token: {
     type: String,
   },
 });
+
+
+userSchema.pre('save',async function(){
+    const salt = await bcrypt.genSalt(10)
+
+    this.password = await bcrypt.hash(this.password, salt)
+ 
+})
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+  
 
 module.exports = mongoose.model("User", userSchema);
